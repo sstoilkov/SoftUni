81458003180c857128fdc31d15e9d4310b8392b6
Svetlozar <svetly91@gmail.com>
2021-09-13 17:12:20 +0300

    New files

---
+++ JS-Advance/old/Arrays_Nested/printArray.js
@@ -0,0 +1,14 @@
+function solver(input, delimiter){
+
+    let result = input.join(`${delimiter}`)
+    console.log(result);
+}
+
+solver(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_'
+)
