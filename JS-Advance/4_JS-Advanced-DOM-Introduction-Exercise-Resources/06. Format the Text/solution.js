e8843ff91aa23905c3fdce4aef0c02a1909db3a0
Svetlozar <53934110+sstoilkov@users.noreply.github.com>
2021-10-06 16:17:24 +0300

    changes

--- JS-Advance/4_JS-Advanced-DOM-Introduction-Exercise-Resources/06. Format the Text/solution.js
+++ JS-Advance/4_JS-Advanced-DOM-Introduction-Exercise-Resources/06. Format the Text/solution.js
@@ -1,3 +1,19 @@
 function solve() {
-  //TODO
+  //1.Select elements
+  let textAreaElement = document.getElementById('input');
+
+  //2.Extract text from text area
+  let text = textAreaElement.value;
+
+  //3. Split text by '.' to get sentances
+  let sentases = text.split('.').filter(x => x != '').map(x => x + '.');
+
+  //4. Group sentances by 3 in paragraphs
+  //5. Insert paragraphs into DOM
+  let resultDiv = document.getElementById('output');
+
+  let paragraphRoof = Math.ceil(sentases.length / 3);
+  for (let i = 0; i < paragraphRoof; i++) {
+    resultDiv.innerHTML += `<p>${sentases.splice(0, 3).join('')}</p>`
+  }
 }
