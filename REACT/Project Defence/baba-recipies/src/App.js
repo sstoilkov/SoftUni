6b862e8fcd23fcf5b745fae6dd7383b846feb467
Svetlozar <svetly91@gmail.com>
2022-07-27 15:05:39 +0300

    init
    React App initialization and clearing

---
+++ REACT/Project Defence/baba-recipies/src/App.js
@@ -0,0 +1,16 @@
+import { Navigation } from "./components/Navigation/Navigation";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="logo-header">Logo here</div>
+        <nav className="header-nav">
+          <Navigation />
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT/Project Defence/baba-recipies/src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+
+export const Navigation = () => {
+
+    return (
+        <>
+            <div>Catalog</div>
+
+            <div>Register</div>
+            <div>Login</div>
+        </>
+    );
+}
